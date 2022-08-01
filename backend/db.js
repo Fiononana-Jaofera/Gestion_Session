42f990e8ed5d1d0fdfc7f14e9ce8ebd0dff6fc51
Fiononana-Jaofera <fiononana.andriatsalama@gmail.com>
2022-08-01 07:56:58 +0300

    insertion guard and test verification email and password

--- backend/db.js
+++ backend/db.js
@@ -2,10 +2,8 @@ let mysql = require('mysql')
 let con = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "Mysql123#",
-    database: "mydb"
+    password: "Jaofera@3110",
+    database: "adminList"
 })
-
-
 
 module.exports=con;
--- backend/index.js
+++ backend/index.js
@@ -3,22 +3,26 @@ const con = require('./db')
 const port = process.env.PORT || 3000
 
 const server = http.createServer((req, res)=>{
-    if(req.url === '/'){
-        res.writeHead(200, {
-            'Content-Type': 'text/html',
+    if(req.url === '/login'){
+        let data = ''
+        req.on('data', chunk => {
+            console.log(`Data chunk available: ${chunk}`)
+            data += chunk
         })
-        res.write('<h1>main work</h1>')
-        res.end()
-    }
-    else if(req.url === '/list'){
-        con.connect((err)=>{
-            if(err) throw err
-            con.query("SELECT * FROM user", (err, result, fields)=>{
-                if(err) throw err;
-                console.log(result)
+        req.on('end', () => {
+            console.log(JSON.parse(data))
+            con.connect((err)=>{
+                if(err) throw err
+                con.query(
+                    "SELECT * FROM adminList",
+                    (err, result, fields) => {
+                        if(err) throw err
+                        console.log(result)
+                    }
+                )
             })
+            res.end()
         })
-        res.end()
     }
     else{
         res.writeHead(400)
